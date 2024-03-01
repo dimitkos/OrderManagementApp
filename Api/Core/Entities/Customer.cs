615fc697655e17902967c0876d103e32d4ff76a0
ΔΗΜΗΤΡΙΟΣ ΚΟΣΜΑΣ <dimitkos@yahoo.gr>
2024-03-01 22:07:23 +0200

    Add the entities

---
+++ Api/Core/Entities/Customer.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+
+namespace Core.Entities
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string ContactNumber { get; set; }
+        public string Email { get; set; }
+        public bool IsDeleted { get; set; }
+        public Address Address { get; set; }
+        public ICollection<Order> Orders { get; set; }
+    }
+}
---
+++ Api/Core/Enums/Status.cs
@@ -0,0 +1,10 @@
+namespace Core.Enums
+{
+    public enum Status
+    {
+        PENDING,
+        DRAFT,
+        COMPLETED,
+        SHIPPED
+    }
+}
